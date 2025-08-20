08f41ea50e0c144dc9016848c43c18144e6831f1
bluff-ayuu <akumarsinha508@gmail.com>
2025-08-20 11:41:14 +0530

    Update lists.py

--- Data_structures/lists.py
+++ Data_structures/lists.py
@@ -1,1 +1,17 @@
+# 1. Does list maintain insertion order?
 
+my_list = [10, 20, 30, 40]
+print("Original list: ", my_list)
+
+
+# 2. Can we access list items with negative index?
+
+my_list = [10, 20, 30, 40]
+print("Last item using negative index:", my_list[-1]) 
+print("Second last item: ", my_list[-2]) 
+
+# 3. Which function empties the list?
+
+my_list = [1, 2, 3, 4]
+my_list.clear()
+print("List after clear(): ", my_list)
